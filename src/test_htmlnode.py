9bbaa51d6d5b2a1043713fb076f28df468def964
HawkEye1983 <git@hawkeye.aleeas.com>
2025-05-13 23:15:50 -0600

    Added ParentNode and tests.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -31,6 +31,28 @@ class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_error(self):
         with self.assertRaises(ValueError):
             LeafNode("a", None)
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+    
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(), "<div><span><b>grandchild</b></span></div>",
+        )
+    
+    def test_to_html_value_error(self):
+        with self.assertRaises(ValueError):
+            ParentNode(None, [LeafNode("i", "Italic text")])
+    
+    def test_to_html_no_children(self):
+        with self.assertRaises(ValueError):
+            ParentNode("a", None)
 
 if __name__ == "__main__":
     unittest.main()

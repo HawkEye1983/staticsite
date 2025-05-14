bdbd5f08c34b9012d67ab529b02776c510f712d0
HawkEye1983 <git@hawkeye.aleeas.com>
2025-05-13 23:39:57 -0600

    Added abilit to convert text to html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -50,3 +50,19 @@ class ParentNode(HTMLNode):
         children_html = ''.join(child.to_html() for child in self.children)
         props_str = self.props_to_html()
         return f"<{self.tag}{props_str}>{children_html}</{self.tag}>"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.NORMAL:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    else:
+        return ValueError("Unsupported TextType")
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,8 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html_node
+from textnode import TextNode, TextType
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -53,6 +55,37 @@ class TestParentNode(unittest.TestCase):
     def test_to_html_no_children(self):
         with self.assertRaises(ValueError):
             ParentNode("a", None)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_normal(self):
+        text_node = TextNode("This is normal", TextType.NORMAL)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "This is normal")
+    
+    def test_bold(self):
+        text_node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<b>This is bold</b>")
+    
+    def test_italic(self):
+        text_node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<i>This is italic</i>")
+    
+    def test_code(self):
+        text_node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<code>This is code</code>")
+    
+    def test_link(self):
+        text_node = TextNode("This is a link", TextType.LINK, "https://boot.dev")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), '<a href="https://boot.dev">This is a link</a>')
+    
+    def test_img(self):
+        text_node = TextNode("This is an image", TextType.IMAGE, "https://boot.dev/image.jpg")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), '<img src="https://boot.dev/image.jpg" alt="This is an image" />')
 
 if __name__ == "__main__":
     unittest.main()
